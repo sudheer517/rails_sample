477802a899bd746a18a1b0c11662d4eaa977b889
harirordeveloper <harijobmail@gmail.com>
2012-07-27 21:42:36 +0530

    Initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sudheer::Application.config.secret_token = '0458546d78a0e21fe5ae4ae4e27d3490dab5533cb27876d87b7c4edc207e603f490d241675569323c04696cebc6383caa44d3598d571171f53259d1fab6ab48e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sudheer::Application.config.session_store :cookie_store, :key => '_sudheer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sudheer::Application.config.session_store :active_record_store
